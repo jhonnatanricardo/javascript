70939a56e27211d48dc960f4afd0d0ce592a990c
Jhonnatan Ricardo Semler <jhonnatanricardo@gmail.com>
2020-07-11 00:58:07 -0300

    Lesson 4

--- js/principal.js
+++ js/principal.js
@@ -36,3 +36,39 @@ for (var i = 0; i < pacientes.length; i++){
         paciente.classList.add("paciente-invalido");
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTR = document.createElement("tr");
+
+    var nomeTD = document.createElement("td");
+    var pesoTD = document.createElement("td");
+    var alturaTD = document.createElement("td");
+    var gorduraTD = document.createElement("td");
+    var imcTD = document.createElement("td");
+
+    nomeTD.textContent = nome;
+    pesoTD.textContent = peso;
+    alturaTD.textContent = altura;
+    gorduraTD.textContent = gordura;
+    imcTD.textContent = (peso / (altura * altura)).toFixed(2);
+    
+    pacienteTR.appendChild(nomeTD);
+    pacienteTR.appendChild(pesoTD);
+    pacienteTR.appendChild(alturaTD);
+    pacienteTR.appendChild(gorduraTD);
+    pacienteTR.appendChild(imcTD);
+
+
+    document.querySelector("#tabela-pacientes").appendChild(pacienteTR);
+
+});
