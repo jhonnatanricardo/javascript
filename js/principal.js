f0c137d41a59f5cb19b60c7fa7edca4ce3b70e3d
Jhonnatan Ricardo Semler <jhonnatanricardo@gmail.com>
2020-07-11 00:15:00 -0300

    Lesson 3, Activity 10

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,38 @@
-document.querySelector('.titulo').textContent="New Title";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++){
+    paciente = pacientes[i];
+
+    var celulaPeso = paciente.querySelector(".info-peso");
+    var peso = celulaPeso.textContent;
+
+    var celulaAltura = paciente.querySelector(".info-altura");
+    var altura = celulaAltura.textContent;
+
+    var imc = peso / (altura * altura);
+
+    var celulaIMC = paciente.querySelector(".info-imc");
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if(peso<=0 || peso >=1000){
+        console.log("Peso inválido!");
+        pesoValido = false;
+        celulaIMC.textContent = "Peso inválido!";
+    }
+
+    if(altura<=0 || altura >=3.00){
+        console.log("Altura inválida!");
+        alturaValida = false;
+        celulaIMC.textContent = "Altura inválida!";
+    }
+
+    if(pesoValido && alturaValida){
+        celulaIMC.textContent = imc.toFixed(2);
+        console.log(imc.toFixed(2));
+    } else {
+        paciente.classList.add("paciente-invalido");
+    }
+}
